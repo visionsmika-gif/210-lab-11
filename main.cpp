d01b20e6e2fb2d6e667572a7bcc811896a12d425
visionsmika-gif <visionsmika@gmail.com>
2025-09-17 00:38:11 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,3 @@
+// Program features a simple dynamic array of structs.
+// The struct definition itself should ALSO include a dynamic array, + other member variables.
+// Model the program off of a real-world scenario.
